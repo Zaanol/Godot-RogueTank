d2cfa12590beac52205bac58bd6cdf13eaf8d266
Willian Zanol <willianzanoll@hotmail.com>
2021-02-07 20:13:58 -0300

    Shot animation + updates

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
 var vel = 300
-var dir = Vector2(0, -1)
+var dir = Vector2(0, -1) setget set_dir
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _process(delta):
 	translate(dir * delta * vel)
 	pass
+
+func _on_Notifier_screen_exited():
+	queue_free()
+
+func set_dir(val):
+	dir = val
+	rotation = dir.angle()
--- Scripts/Tank.gd
+++ Scripts/Tank.gd
@@ -23,9 +23,14 @@ func _process(delta):
 		dirY -= 1
 		
 	if Input.is_action_just_pressed("ui_shoot"):
-		var bullet = preBullet.instance()
-		bullet.global_position = $Barrel/Muzzle.global_position
-		get_parent().add_child(bullet)
+		if get_tree().get_nodes_in_group("CannonBullets").size() < 6:
+			var bullet = preBullet.instance()
+			bullet.global_position = $Barrel/Muzzle.global_position
+			bullet.dir = Vector2(cos(rotation), sin(rotation)).normalized()
+			$Barrel/Anim.play("Fire")
+			get_parent().add_child(bullet)
+	
+	look_at(get_global_mouse_position())
 	
 	translate(Vector2(dirX, -dirY) * delta * speed)
 	pass
